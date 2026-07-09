237d7e9e70bf2570215fd057ed5e1d5a31d25036
Carl Steven Vinluan <carlstevenjvinluan@iskolarngbayan.pup.edu.ph>
2026-07-10 05:07:30 +0800

    Fix Navbar case-sensitivity

--- src/components/RightPanel.tsx
+++ src/components/RightPanel.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react'
 import { PROJECTS, ARTISTS, NEWS, Project, Artist, NewsItem, getCoverUrl, formatTimeAgo } from '@/data/projects'
-import Navbar from '@/components/NavBar'
+import NavBar from '@/components/NavBar'
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://YOUR_PROJECT_ID.supabase.co'
 const LOGO_URL = `${SUPABASE_URL.replace(/\/$/, '')}/storage/v1/object/public/misc/ss7.png`
@@ -77,7 +77,7 @@ export default function RightPanel({ currentView, setCurrentView, onOpenModal, o
 
   return (
     <div className="right-panel">
-      <Navbar currentView={currentView} setCurrentView={setCurrentView} onOpenMenu={onOpenMenu} />
+      <NavBar currentView={currentView} setCurrentView={setCurrentView} onOpenMenu={onOpenMenu} />
       <main className="main-content">
         
         {currentView === 'home' && (
